a140ba64fe2b409f6047f7d6ae4bdd268d18528e
Jeremie Castagna <jcastagna@twitter.com>
2013-02-22 10:51:52 -0800

    error handling broken out into module

--- lib/gin/controller.rb
+++ lib/gin/controller.rb
@@ -1,6 +1,7 @@
 class Gin::Controller
   extend GinClass
   include Gin::Filterable
+  include Gin::Errorable
 
   ##
   # String representing the controller name.
@@ -13,66 +14,7 @@ class Gin::Controller
   end
 
 
-  ##
-  # Define an error handler for this Controller. Configurable with exceptions
-  # or status codes. Omitting the err_types argument acts as a catch-all for
-  # non-explicitly handled errors.
-  #
-  #   error 502, 503, 504 do
-  #     # handle unexpected upstream error
-  #   end
-  #
-  #   error do |err|
-  #     # catch-all
-  #   end
-  #
-  #   error Timeout::Error do |err|
-  #     # something timed out
-  #   end
-
-  def self.error *err_types, &block
-    return unless block_given?
-    err_types << nil if err_types.empty?
-
-    err_types.each do |name|
-      self.error_handlers[name] = block
-    end
-  end
-
-
-  ##
-  # Run after an error has been raised and optionally handled by an
-  # error callback. The block will get run on all errors and is given
-  # the exception instance as an argument.
-
-  def self.all_errors &block
-    return unless block_given?
-    self.error_handlers[:all] = block
-  end
-
-
-  ##
-  # Hash of error handlers defined by Gin::Controller.error.
-  # This attribute is inherited.
-
-  def self.error_handlers
-    @err_handlers ||= self.superclass.respond_to?(:error_handlers) ?
-                        self.superclass.error_handlers.dup : {}
-  end
-
-
-  ##
-  # Set or get the default content type for this Gin::Controller.
-  # Default value is "text/html". This attribute is inherited.
-
-  def self.content_type new_type=nil
-    return @content_type = new_type if new_type
-    @content_type ||= self.superclass.respond_to?(:content_type) ?
-                        self.superclass.content_type.dup : "text/html"
-  end
-
-
-  class_proxy_reader :controller_name, :err_handlers
+  class_proxy_reader :controller_name
 
   attr_reader :app, :request, :response, :action
 
@@ -97,22 +39,6 @@ class Gin::Controller
 
   rescue => err
     handle_error err
-  end
-
-
-  ##
-  # Calls the appropriate error handlers for the given error.
-  # Re-raises the error if no handler is found.
-
-  def handle_error err
-    key = self.error_handlers.keys.find do |key|
-            key === err || err.respond_to?(:status) && key === err.status
-          end
-
-    raise err unless key || self.error_handlers[:all]
-
-    instance_exec(err, &self.error_handlers[key])  if key
-    instance_exec(err, &self.error_handlers[:all]) if self.error_handlers[:all]
   end
 
 
