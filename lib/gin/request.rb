0b2cbd3daaa9da0e52054765301991b0353e400e
Jeremie Castagna <jcastagna@twitter.com>
2013-02-23 12:27:10 -0800

    indifferent access params

--- lib/gin/request.rb
+++ lib/gin/request.rb
@@ -4,4 +4,37 @@ class Gin::Request < Rack::Request
     super
     self.params.update env['gin.path_query_hash']
   end
+
+
+  def params
+    super
+    @params = indifferent_params @params
+  end
+
+
+  private
+
+  ##
+  # Enable string or symbol key access to the nested params hash.
+
+  def indifferent_params(object)
+    case object
+    when Hash
+      new_hash = indifferent_hash
+      object.each { |key, value| new_hash[key] = indifferent_params(value) }
+      new_hash
+    when Array
+      object.map { |item| indifferent_params(item) }
+    else
+      object
+    end
+  end
+
+
+  ##
+  # Creates a Hash with indifferent access.
+
+  def indifferent_hash
+    Hash.new {|hash,key| hash[key.to_s] if Symbol === key }
+  end
 end
