6a3375281e4b24b0d97715c2bace20438418d369
Jeremie Castagna <jcastagna@twitter.com>
2013-09-19 16:42:43 -0700

    cleanup

--- lib/gin/asset_pipeline.rb
+++ lib/gin/asset_pipeline.rb
@@ -7,7 +7,6 @@ class Gin::AssetPipeline
   attr_reader   :render_dir, :name, :asset_paths
 
   def initialize name, render_dir, asset_paths, &block
-    @name       = name
     @rendering  = 0
     @logger     = $stderr
     @listen     = false
@@ -15,13 +14,17 @@ class Gin::AssetPipeline
     @last_mtime = Time.now
     @render_dir = nil
     @sprockets  = nil
+    @name       = nil
     @flag_update = false
     @asset_paths = nil
 
     @render_lock = Gin::RWLock.new
     @listen_lock = Gin::RWLock.new
 
-    self.render_dir = render_dir
+    @render_dir = render_dir
+    self.name = name
+
+    load_assets_version
     setup_listener(asset_paths, &block)
   end
 
@@ -106,11 +109,26 @@ class Gin::AssetPipeline
   end
 
 
+  def name= new_name
+    @listen_lock.write_sync do
+      old_vfile = assets_version_file
+
+      @name = new_name
+      new_vfile = assets_version_file
+
+      FileUtils.mv(old_vfile, new_vfile) if
+        File.file?(old_vfile) && old_vfile != new_vfile
+      @name
+    end
+  end
+
+
   def render_dir= new_dir
     new_dir = File.expand_path(new_dir)
 
     if !@render_dir || @render_dir != new_dir
       @listen_lock.write_sync do
+        # TODO: instead of re-rendering everything, maybe move rendered assets?
         @flag_update = true
         @render_dir  = new_dir
         load_assets_version
@@ -181,7 +199,7 @@ class Gin::AssetPipeline
   end
 
 
-  def logical_path_for path
+  def local_path_for path
     path = path.sub(@render_dir,'')
     path.sub!(/^\//, '')
     path.sub!(/-[0-9a-f*]+(\.\w+)$/i, '\1')
@@ -208,7 +226,8 @@ class Gin::AssetPipeline
     @render_lock.write_sync{ @rendering += 1 }
 
     unless assets_version_outdated?
-      log "No assets to update"
+      log "No assets to update. \
+Delete the version file to force asset rendering:\n  #{assets_version_file}\n"
       return
     end
 
@@ -218,8 +237,9 @@ class Gin::AssetPipeline
 
     Dir[dir_glob].each do |path|
       next unless File.file?(path)
-      logical_path = logical_path_for(path)
-      remove_path path if !@sprockets[logical_path]
+      next unless local_path = local_path_for(path)
+      valid_asset = @sprockets.resolve(local_path) rescue false
+      remove_path path if !valid_asset
     end
 
     @sprockets.each_logical_path do |path|
